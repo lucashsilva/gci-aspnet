9439948284a265bcbe9b0fedca2a12881e0e2ba7
Lucas Silva <lucashenrique125@gmail.com>
2020-03-23 20:26:44 -0300

    Add GCIMiddleware.cs - C# ASP.NET implementation of GCI as a middleware

---
+++ GCIMiddleware.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Threading.Tasks;
+
+namespace GCI
+{
+    public class GCIMiddleware
+    {
+        RequestDelegate _next;
+
+        public GCIMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            if (context.Request.Path.Equals("/__gci"))
+            {
+                string header = context.Request.Headers["gci"];
+
+                switch (header)
+                {
+                    case "ch":
+                        Console.WriteLine("Heap usage check at " + DateTimeOffset.Now.ToString());
+                        await context.Response.WriteAsync(GC.GetTotalMemory(false).ToString());
+                        break;
+
+                    case "gc":
+                        Console.WriteLine("GC: " + DateTimeOffset.Now.ToString());
+                        GC.Collect();
+                        break;
+                }
+
+                await context.Response.CompleteAsync();
+            }
+            else
+            {
+                await _next(context);
+            }
+        }
+    }
+}
